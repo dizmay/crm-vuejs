2bf0386ced69903c68999f2da79e996c2c14e812
dizmay <piotr.stukalau@itechart-group.com>
2021-09-27 12:55:47 +0300

    feat: add vue components and routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,52 @@ const routes = [
     component: () => import("@/views/Login.vue"),
   },
   {
+    path: "/register",
+    name: "Register",
+    meta: { layout: "auth" },
+    component: () => import("@/views/Register.vue"),
+  },
+  {
+    path: "/",
+    name: "Home",
+    meta: { layout: "main" },
+    component: () => import("@/views/Home.vue"),
+  },
+  {
     path: "/categories",
     name: "Categories",
     meta: { layout: "main" },
     component: () => import("@/views/Categories.vue"),
+  },
+  {
+    path: "/detail",
+    name: "Detail",
+    meta: { layout: "main" },
+    component: () => import("@/views/Detail.vue"),
+  },
+  {
+    path: "/history",
+    name: "History",
+    meta: { layout: "main" },
+    component: () => import("@/views/History.vue"),
+  },
+  {
+    path: "/planning",
+    name: "Planning",
+    meta: { layout: "main" },
+    component: () => import("@/views/Planning.vue"),
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    meta: { layout: "main" },
+    component: () => import("@/views/Profile.vue"),
+  },
+  {
+    path: "/record",
+    name: "Record",
+    meta: { layout: "main" },
+    component: () => import("@/views/Record.vue"),
   },
 ];
 
