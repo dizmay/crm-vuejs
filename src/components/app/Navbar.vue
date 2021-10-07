7041a41145d61c7f74c6e6330eb14379870cb424
dizmay <piotr.stukalau@itechart-group.com>
2021-10-07 13:20:22 +0300

    feat: add dropdown func & date

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,8 +5,74 @@
         <a href="#" @click.prevent="$emit('burgerClicked')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ formattedDate }}</span>
       </div>
+
+      <ul class="right hide-on-small-and-down">
+        <li>
+          <a
+            class="dropdown-trigger black-text"
+            href="#"
+            data-target="dropdown"
+            ref="dropdown"
+          >
+            USERNAME
+            <i class="material-icons right">arrow_drop_down</i>
+          </a>
+
+          <ul id="dropdown" class="dropdown-content">
+            <li>
+              <router-link to="/profile" class="black-text">
+                <i class="material-icons">account_circle</i>Профиль
+              </router-link>
+            </li>
+            <li class="divider" tabindex="-1"></li>
+            <li>
+              <a href="#" class="black-text" @click.prevent="logout">
+                <i class="material-icons">assignment_return</i>Выйти
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
+
+<script>
+import moment from "moment";
+export default {
+  data() {
+    return {
+      date: new Date(),
+      interval: null,
+      dropdown: null,
+    };
+  },
+  methods: {
+    logout() {
+      console.log("LOGOUT");
+      this.$router.push("/login?message=logout");
+    },
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+    this.dropdown = window.M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false,
+    });
+  },
+  computed: {
+    formattedDate() {
+      return moment(this.date).format("MMMM Do, h:mm:ss");
+    },
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
+  },
+};
+</script>
